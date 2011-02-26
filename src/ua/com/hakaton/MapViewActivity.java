682ac580b7b4d0d4d7d87a5d2094248492fe2517
Alexey <leopard.not.a@gmail.com>
2011-02-26 16:00:27 +0200

    Overlay

--- src/ua/com/hakaton/MapViewActivity.java
+++ src/ua/com/hakaton/MapViewActivity.java
@@ -15,7 +15,6 @@ public class MapViewActivity extends MapActivity {
 	protected Drawable drawable;
 	protected RoadItemizedOverlay itemizedOverlay;	
 	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -24,24 +23,7 @@ public class MapViewActivity extends MapActivity {
         setContentView(R.layout.map);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
-
-        
-        JsonAgregator jsonAgregator = new JsonAgregator();
-        jsonAgregator.setMarkers(this, mapView);
-        
-//        mapOverlays = mapView.getOverlays();
-//        drawable = this.getResources().getDrawable(android.R.drawable.ic_delete);
-//        itemizedOverlay = new RoadItemizedOverlay(drawable, this); 
-//        
-//        GeoPoint point = new GeoPoint(19240000,-99120000);
-//        RoadOverlayItem overlayitem = new RoadOverlayItem(point, "dg dg dddfgdfgdfg dgd ", "Snippet");
-//        itemizedOverlay.addOverlay(overlayitem);
-//
-//        GeoPoint point2 = new GeoPoint(35410000, 139460000);
-//        RoadOverlayItem overlayitem2 = new RoadOverlayItem(point2, "", "");
-//        itemizedOverlay.addOverlay(overlayitem2);
-//        
-//        mapOverlays.add(itemizedOverlay);	
+        JsonAgregator.setMarkers(this, mapView);
 	}
 
 
