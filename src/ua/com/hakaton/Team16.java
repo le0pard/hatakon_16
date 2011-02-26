3951e212b43ebe393090d11a75acde910aef3af5
Alexey <leopard.not.a@gmail.com>
2011-02-26 13:29:51 +0200

    init commit

---
+++ src/ua/com/hakaton/Team16.java
@@ -0,0 +1,13 @@
+package ua.com.hakaton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Team16 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
