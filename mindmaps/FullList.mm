bd92cb81262932e86ea5ddea4b170a1b7bce1316
g-r-i-n <g-r-i-n@yandex.ru>
2011-03-09 15:19:32 +0200

    Основные MindMap-доки добавлены

---
+++ mindmaps/FullList.mm
@@ -0,0 +1,13 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x41f;&#x43e;&#x43b;&#x43d;&#x44b;&#x439; &#x441;&#x43f;&#x438;&#x441;&#x43e;&#x43a;" ID="ID_860738583" CREATED="1294235447827" MODIFIED="1294779835294" COLOR="#666600" BACKGROUND_COLOR="#ff99ff">
+<font SIZE="16" BOLD="true"/>
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="RoadMap UI" POSITION="right" ID="ID_460857967" CREATED="1298719072564" MODIFIED="1299354539492" LINK="RoadMapUI.mm"/>
+<node TEXT="&#x41d;&#x430;&#x447;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x435; &#x438;&#x43d;&#x441;&#x442;&#x440;&#x443;&#x43a;&#x446;&#x438;&#x438;" POSITION="left" ID="ID_857406458" CREATED="1299352800041" MODIFIED="1299352812374"/>
+<node TEXT="Usefull tips" POSITION="left" ID="ID_1854575071" CREATED="1299352825455" MODIFIED="1299352840757"/>
+<node TEXT="PA3HOE" POSITION="left" ID="ID_1560455094" CREATED="1299352848420" MODIFIED="1299352880991"/>
+<node TEXT="RoadMap Analytics" POSITION="right" ID="ID_474936151" CREATED="1299354491780" MODIFIED="1299354626858" LINK="RoadMapAnalytics.mm"/>
+<node TEXT="Resources" POSITION="right" ID="ID_970350305" CREATED="1299352892767" MODIFIED="1299353919782" LINK="Resources.mm"/>
+</node>
+</map>
---
+++ mindmaps/RoadMapAnalytics.mm
@@ -0,0 +1,14 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="RoadMap Analytics" ID="ID_860738583" CREATED="1294235447827" MODIFIED="1299354603866" COLOR="#666600" BACKGROUND_COLOR="#ff99ff">
+<font SIZE="16" BOLD="true"/>
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="PA3HOE" POSITION="left" ID="ID_1560455094" CREATED="1299352848420" MODIFIED="1299352880991"/>
+<node TEXT="&#x41f;&#x440;&#x43e;&#x431;&#x43a;&#x438;" POSITION="right" ID="ID_474936151" CREATED="1299354491780" MODIFIED="1299354996399"/>
+<node TEXT="&#x422;&#x440;&#x44f;&#x441;&#x43a;&#x438;" POSITION="right" ID="ID_838302776" CREATED="1299354998517" MODIFIED="1299355001026"/>
+<node TEXT="&#x41e;&#x441;&#x442;&#x430;&#x43d;&#x43e;&#x432;&#x43a;&#x438;" POSITION="right" ID="ID_1357245227" CREATED="1299355002574" MODIFIED="1299355032209"/>
+<node TEXT="&#x41f;&#x440;&#x438;&#x442;&#x43e;&#x440;&#x43c;&#x430;&#x436;&#x438;&#x432;&#x430;&#x43d;&#x438;&#x44f;" POSITION="right" ID="ID_476123484" CREATED="1299355054514" MODIFIED="1299355057932"/>
+<node TEXT="&#x41f;&#x43e;&#x432;&#x43e;&#x440;&#x43e;&#x442;&#x44b;" POSITION="right" ID="ID_1325957003" CREATED="1299355046556" MODIFIED="1299355049858"/>
+<node TEXT="&#x41f;&#x43e;&#x434;&#x432;&#x435;&#x441;&#x43a;&#x438;" POSITION="right" ID="ID_961143352" CREATED="1299355039392" MODIFIED="1299355043928"/>
+</node>
+</map>
